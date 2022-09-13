dbca7d4b6f8fe477225266eb97dcb7ee2777769e
Idris Yakub <olayinkayakub@yahoo.com>
2022-09-13 04:53:25 +0100

    write a program to prints number in base 10, from 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * description: prints number in base 10, from 0 - 9
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+putchar(n + '0');
+}
+
+putchar('\n');
+
+return (0);
+
+}
