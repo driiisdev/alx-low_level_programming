7d5841e41b838102e6c34f5d30a9e3c350541d57
Idris Yakub <olayinkayakub@yahoo.com>
2022-09-18 09:59:25 +0100

    function that prints 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,8 @@
 
 /**
  * print_most_numbers - Print numbers 0 to 9, excluding 2 and 4.
- * Description: Can only use _putchar twice.
+ * @c: do not print 2 and 4
+ * @c: Can only use _putchar twice.
  */
 void print_most_numbers(void)
 {
