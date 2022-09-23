776ceab80a69c8ce294f6317be69a85cae7c9f22
Idris Yakub <olayinkayakub@yahoo.com>
2022-09-23 04:50:39 +0100

    function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,28 +8,29 @@
 
 void rev_string(char *s)
 {
-	int i;
-	char c, *start, *end;
-	int length;
 
-	start = s;
-	end = s;
+int i;
+char c, *start, *end;
+int length;
 
-	for (i = 0; s[i]; i++)
-		;
-	length = i;
+start = s;
+end = s;
 
-	for (i = 0; i < length - 1; i++)
-		end++;
-	for (i = 0; i < length / 2; i++)
-	{
-		c = *end;
-		*end = *start;
-		*start = c;
+for (i = 0; s[i]; i++)
+;
+length = i;
 
-		start++;
-		end--;
-	}
+for (i = 0; i < length - 1; i++)
+end++;
+for (i = 0; i < length / 2; i++)
+{
+c = *end;
+*end = *start;
+*start = c;
+
+start++;
+end--;
+}
 }
 
 /**
@@ -40,23 +41,22 @@ void rev_string(char *s)
 * @size_r: buffer
 * Return: r if success
 */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, sum1;
-	int n1len;
+int i, sum1;
+int n1len;
 
-	for (i = 0; n1; i++)
-		;
-		n1len = i;
+for (i = 0; n1; i++)
+;
+n1len = i;
 
-	rev_string(n1);
-	rev_string(n2);
+rev_string(n1);
+rev_string(n2);
 
-	for (i = 0; n1len; i++)
-	{
-		sum1 = ((n1[i] - '0') + (n2[i] - '0') + size_r);
-		r[i] = sum1;
-	}
-	return (r);
+for (i = 0; n1len; i++)
+{
+sum1 = ((n1[i] - '0') + (n2[i] - '0') + size_r);
+r[i] = sum1;
+}
+return (r);
 }
