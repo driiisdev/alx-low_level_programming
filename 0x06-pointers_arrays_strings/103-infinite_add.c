589a704324b506e0dda3090852a1872c5ed80078
Idris Yakub <olayinkayakub@yahoo.com>
2022-09-23 05:10:32 +0100

    function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,13 +5,11 @@
  * @n2: second number
  * @r: buffer for result
  * @size_r: buffer size
- *
  * Return: address of r or 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 int i, j, k, l, m, n;
-
 for (i = 0; n1[i]; i++)
 ;
 for (j = 0; n2[j]; j++)
