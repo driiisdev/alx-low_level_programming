d4d1691455355f3e6f14e01ad04cfd9a7331ce8c
Idris Yakub <olayinkayakub@yahoo.com>
2022-09-23 05:07:16 +0100

    function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,62 +1,52 @@
 #include "main.h"
 /**
-* rev_string - modify the string to be reversed
-* @s: string to reverse
-*
-* Return: void
-*/
-
-void rev_string(char *s)
-{
-
-int i;
-char c, *start, *end;
-int length;
-
-start = s;
-end = s;
-
-for (i = 0; s[i]; i++)
-;
-length = i;
-
-for (i = 0; i < length - 1; i++)
-end++;
-for (i = 0; i < length / 2; i++)
-{
-c = *end;
-*end = *start;
-*start = c;
-
-start++;
-end--;
-}
-}
-
-/**
-* infinite_add - add a seemingly infinite two numbers
-* @n1: first number
-* @n2: second number
-* @r: sum of both numbers
-* @size_r: buffer
-* Return: r if success
-*/
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
+ *
+ * Return: address of r or 0
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i, sum1;
-int n1len;
+int i, j, k, l, m, n;
 
-for (i = 0; n1; i++)
-;
-n1len = i;
-
-rev_string(n1);
-rev_string(n2);
-
-for (i = 0; n1len; i++)
+for (i = 0; n1[i]; i++);
+for (j = 0; n2[j]; j++);
+if (i > size_r || j > size_r)
 {
-sum1 = ((n1[i] - '0') + (n2[i] - '0') + size_r);
-r[i] = sum1;
+return (0);
+}
+m = 0;
+for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+{
+n = m;
+if (i >= 0)
+{
+n += n1[i] - '0';
+}
+if (j >= 0)
+{
+n += n2[j] - '0';
+}
+if (i < 0 && j < 0 && n == 0)
+{
+break;
+}
+m = n / 10;
+r[k] = n % 10 + '0';
+}
+r[k] = '\0';
+if (i >= 0 || j >= 0 || m)
+{
+return (0);
+}
+for (k -= 1, l = 0; l < k; k--, l++)
+{
+m = r[k];
+r[k] = r[l];
+r[l] = m;
 }
 return (r);
 }
