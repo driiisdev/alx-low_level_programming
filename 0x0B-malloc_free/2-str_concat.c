568b63b6ea0b930e70b03d9860c2ac6a73bcce7c
Idris Yakub <olayinkayakub@yahoo.com>
2022-10-04 07:44:58 +0100

    function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,12 +15,12 @@ int j = 0;
 int len1 = 0;
 int len2 = 0;
 
-while (s1 != '0/' && s1[len1])
+while (s1 != '\0' && s1[len1])
 {
 len1++;
 }
 
-while (s2 != '0/' && s2[len2])
+while (s2 != '\0' && s2[len2])
 {
 len2++;
 }
