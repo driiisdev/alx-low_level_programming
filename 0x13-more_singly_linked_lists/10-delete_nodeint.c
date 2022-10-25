fa2a70424ea4b51e621de3c0e0466c78bf5f96ae
Idris Yakub <olayinkayakub@yahoo.com>
2022-10-25 16:24:52 +0100

    function that reverses a listint_t linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdlib.h>
-
 /**
  * delete_nodeint_at_index - deletes the node at certain index
  * of a listint_t linked list.
@@ -8,6 +7,7 @@
  * @index: index of the node to delete
  * Return: 1 (Success), or -1 (Fail)
  */
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 listint_t *tmp = *head;
