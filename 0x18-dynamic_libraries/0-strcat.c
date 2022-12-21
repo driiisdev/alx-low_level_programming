45227c757db1da2572dc232a040897aa7f0cad88
Idris Yakub <olayinkayakub@yahoo.com>
2022-12-21 18:08:11 +0100

    Create the dynamic library libdynamic.so containing all the functions

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,31 @@
+
+
+#include "main.h"
+
+/**
+ * _strcat - concatenates strings
+ * @src: string to be appeneded
+ * @dest: string to be updated by src
+ *
+ * Return: the pointer of dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *d;
+
+	if (*src != '\0')
+	{
+		d = dest;
+		while (*dest != '\0')
+		{
+			dest++;
+		}
+		while (*src != '\0')
+		{
+			*dest = *src;
+			dest++;
+			src++;
+		}
+	}
+	return (d);
+}
