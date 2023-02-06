b60ba537b8d181aea2cecd1fb7935216edeb7ee0
Idris Yakub <olayinkayakub@yahoo.com>
2023-02-06 02:47:45 +0100

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ./ https://github.com/driiisdev/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
-export LD_PRELOAD=./libmask.so
+wget -P /tmp https://github.com/driiisdev/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/libmask.so
